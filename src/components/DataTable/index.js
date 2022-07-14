70c5a8650957daf07170011b365584ef666ed1ef
Danilo Masato <danilomasato@hotmail.com>
2022-07-14 00:04:47 -0300

    removendo errros e consoles

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 function Row(props) {
   const { row } = props;
@@ -20,7 +20,7 @@ function Row(props) {
 
   return (
     <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
           <IconButton
             aria-label="expand row"
@@ -55,7 +55,7 @@ function Row(props) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.bank.map((historyRow) => (
+                  {row.bank.map(historyRow => (
                     <TableRow key={historyRow.date}>
                       <TableCell component="th" scope="row">
                         {historyRow.bankName}
@@ -85,87 +85,61 @@ Row.propTypes = {
         bankName: PropTypes.string.isRequired,
         code: PropTypes.string.isRequired,
         agency: PropTypes.string.isRequired,
-        account: PropTypes.string.isRequired,
-      }),
+        account: PropTypes.string.isRequired
+      })
     ).isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
-  }).isRequired,
+    protein: PropTypes.number.isRequired
+  }).isRequired
 };
 
-const DataTable = React.forwardRef(
-  (
-    {
-      ...props
-    }
-  ) => {
-
-    function createData(name, document) {
-      return {
-        name,
-        document,
-        bank: [
-          {
-            bankName: '2020-01-05',
-            code: '11091700',
-            agency: '',
-            account: ''
-          }
-        ],
-      };
-    }
-
-    // const rows = [
-    //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    //   createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    //   createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    //   createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-    // ];
-
-    const [rows, setRows] = useState([]);
-
-    //first load
-    useEffect(() => {
-      if (props.data.length > 0) {
-          console.log('props.data============>', props.data)
-          // setBankdataList(props.data);
-          // setColumns(props.data.filter(item => (
-          //   { field: item.id, headerName: item.id, width: 70 },
-          //   { field: item.name, headerName: item.name, width: 130 }
-          // )))
-          setRows(props.data.filter(item => (
-            // { field: item.id, headerName: item.id, width: 70 },
-            // { field: item.name, headerName: item.name, width: 130 }
-            createData(item.id, item.name, item.bank)
-          )))
-      }
-    }, [props.data])
-
-    
-    // console.log('data===========>', props.data)
-
-    return (
-      <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell align="left"> ID </TableCell>
-              <TableCell align="left"> Nome </TableCell>
-              <TableCell align="left"> documento </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <Row key={row.name} row={row} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+const DataTable = React.forwardRef(({ ...props }) => {
+  function createData(name, document) {
+    return {
+      name,
+      document,
+      bank: [
+        {
+          bankName: "2020-01-05",
+          code: "11091700",
+          agency: "",
+          account: ""
+        }
+      ]
+    };
   }
-)
+
+  const [rows, setRows] = useState([]);
+
+  //first load
+  useEffect(() => {
+    if (props.data.length > 0) {
+      setRows(
+        props.data.filter(item => createData(item.id, item.name, item.bank))
+      );
+    }
+  }, [props.data]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell align="left"> ID </TableCell>
+            <TableCell align="left"> Nome </TableCell>
+            <TableCell align="left"> documento </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+});
 
 export default DataTable;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,55 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Container } from "../components";
-import {
-  getBankData,
-} from "../actions";
+import { getBankData } from "../actions";
 import "./Home.css";
-import DataTable  from "../components/DataTable";
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
+import DataTable from "../components/DataTable";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
-const Home = ({
-  data,
-  bankdata,
-  getBankData
-}) => {
-
-    const [menu, setMenu] = useState([{
-        title: 'Home',
-        action: '/'
-    }]);
+const Home = ({ data, bankdata, getBankData }) => {
+  const [menu, setMenu] = useState([
+    {
+      title: "Home",
+      action: "/"
+    }
+  ]);
 
   return (
     <React.Fragment>
-        <Container menu={menu}>
-             <Alert severity="info">
-                <AlertTitle>Notificação</AlertTitle>
-                Caros usuários, <strong>seus dados bancários</strong> se encontram na setinha no canto esquerdo.
-              </Alert>
+      <Container menu={menu}>
+        <Alert severity="info">
+          <AlertTitle>Notificação</AlertTitle>
+          Caros usuários, <strong>seus dados bancários</strong> se encontram na
+          setinha no canto esquerdo.
+        </Alert>
 
-            <DataTable data={bankdata}/>
-        </Container>
+        <DataTable data={bankdata} />
+      </Container>
     </React.Fragment>
   );
 };
 
 const mapStateToProps = state => ({
   data: state.home,
-  bankdata: state.home.bankdata,
+  bankdata: state.home.bankdata
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      getBankData: dispatch(getBankData()),
+      getBankData: dispatch(getBankData())
     },
     dispatch
   );
 
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(Home)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Home));
